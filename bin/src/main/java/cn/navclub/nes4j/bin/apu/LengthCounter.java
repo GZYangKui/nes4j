d3167732d859dade28510d7aaab15c51c0d23054
yangkui <752544765@qq.com>
2023-10-27 12:41:04 +0800

    :ambulance: fix LengthCounter#lookupTable ArrayIndexOutOfBoundsException issue

--- bin/src/main/java/cn/navclub/nes4j/bin/apu/LengthCounter.java
+++ bin/src/main/java/cn/navclub/nes4j/bin/apu/LengthCounter.java
@@ -3,6 +3,8 @@ package cn.navclub.nes4j.bin.apu;
 import cn.navclub.nes4j.bin.function.CycleDriver;
 import lombok.Data;
 
+import static cn.navclub.nes4j.bin.util.BinUtil.uint8;
+
 @Data
 public class LengthCounter implements CycleDriver {
     private static final int[] LOOKUP_TABLE = {
@@ -71,7 +73,7 @@ public class LengthCounter implements CycleDriver {
      * @param b Register valuer
      */
     public void lookupTable(byte b) {
-        this.counter = LOOKUP_TABLE[b >>> 3];
+        this.counter = LOOKUP_TABLE[uint8(b) >> 3];
     }
 
 
